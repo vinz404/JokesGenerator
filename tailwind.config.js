a80829937cb053b9b953be0dbddcac47d5c73733
vinz <kejoxz@gmail.com>
2024-01-08 21:05:57 +0800

    fix path

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 
 module.exports = {
-  content: ["./public/**/*.{html,js}"],
+  content: ["index.html"],
   theme: {
     extend: {},
     fontFamily: {
